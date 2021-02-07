cbf2626f8c3ff9f1b3577f9c9e5a3e931a0b598f
Andres <andrest2455@gmail.com>
2021-02-06 19:08:20 -0800

    Added a couple of comments and added indentation.

--- average_elements.py
+++ average_elements.py
@@ -1,4 +1,11 @@
+
+"""
+Function for calculating the average of a list
+-Return value of -1 is an error
+"""
 def averageElements(values):
+    
     if(len(values) == 0):
         return -1
+    
     return round(sum(values) / len(values), 2)
--- full_name.py
+++ full_name.py
@@ -1,4 +1,11 @@
+
+"""
+Function for concatenating into a full name
+-Return value of -1 is an error
+"""
 def fullName(first_name, last_name):
+    
     if(not(isinstance(first_name, str) and isinstance(last_name, str))):
         return -1
+    
     return first_name.strip() + " " + last_name.strip()
